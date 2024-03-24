41f2f84ee73ce978bf2807786e5d090ecce34b7a
kyewu <18336768355@163.com>
2024-03-24 22:57:46 +0800

    chore: Project scaffolding

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
-  <main px-4 py-10>
-    <router-view />
-    <div mx-auto mt-5 text-center text-sm opacity-50>
-      [Default Layout]
-    </div>
+  <div flex>
+    <router-link to="/">
+      产品
+    </router-link>
+    <router-link to="/community">
+      社区
+    </router-link>
+    <router-link to="/study">
+      学习
+    </router-link>
+    <router-link to="/about">
+      关于
+    </router-link>
+  </div>
+  <main px-4 py-10 flex-1>
+    Main
+    <RouterView />
   </main>
+  <footer>Footer</footer>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -70,6 +70,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets/*': fileURLToPath(new URL('./src/assets/*', import.meta.url)),
+      '@pages/*': fileURLToPath(new URL('./src/pages/*', import.meta.url)),
+      '@router/*': fileURLToPath(new URL('./src/router/*', import.meta.url)),
+      '@store/*': fileURLToPath(new URL('./src/store/*', import.meta.url)),
+      '@components/*': fileURLToPath(new URL('./src/components/*', import.meta.url)),
     },
   },
 })
