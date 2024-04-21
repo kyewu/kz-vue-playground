825a0d77258cf7f0e2cb054e39f7d860ad406e7f
kyewu <18336768355@163.com>
2024-04-21 17:51:46 +0800

    chore: Temp footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Container>
+    <div>
+      <section>
+        menu
+      </section>
+      <section>
+        collection
+      </section>
+    </div>
+    <div>
+      <div>title</div>
+      <div>content</div>
+    </div>
+  </Container>
+</template>
+
+<style scoped lang="scss">
+
+</style>
