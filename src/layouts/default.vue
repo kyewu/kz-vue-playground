6ba9d090a326d95dba3487f8e2552a0e31288aec
kyewu <18336768355@163.com>
2024-04-22 23:01:48 +0800

    chore: Adjust layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,51 +1,6 @@
 <script setup lang="ts">
 import Logo from '@/assets/imgs/logo.png'
-import type { MenuItemType, SwiperItemType } from '@/components/types'
-import banner1 from '@/assets/imgs/banner/banner_0.jpg'
-import banner2 from '@/assets/imgs/banner/banner_1.jpg'
-import banner3 from '@/assets/imgs/banner/banner_2.jpg'
-import banner4 from '@/assets/imgs/banner/banner_3.jpg'
-import banner5 from '@/assets/imgs/banner/banner_4.jpg'
-import banner6 from '@/assets/imgs/banner/banner_5.jpg'
-
-const items: SwiperItemType[] = [
-  {
-    image: banner1,
-    title: 'title',
-    subTitle: 'subTitle',
-    desc: 'desc',
-  },
-  {
-    image: banner2,
-    title: 'title',
-    subTitle: 'subTitle',
-    desc: 'desc',
-  },
-  {
-    image: banner3,
-    title: 'title',
-    subTitle: 'subTitle',
-    desc: 'desc',
-  },
-  {
-    image: banner4,
-    title: 'title',
-    subTitle: 'subTitle',
-    desc: 'desc',
-  },
-  {
-    image: banner5,
-    title: 'title',
-    subTitle: 'subTitle',
-    desc: 'desc',
-  },
-  {
-    image: banner6,
-    title: 'title',
-    subTitle: 'subTitle',
-    desc: 'desc',
-  },
-]
+import type { MenuItemType } from '@/components/types'
 
 const menuList: MenuItemType[] = [
   { name: '产品', link: '/' },
@@ -58,19 +13,14 @@ const { y } = useWindowScroll()
 </script>
 
 <template>
-  <div fixed top-0 w-full bg-white z-36 :class="{ ' shadow-lg': y > 0 }">
+  <div class="border-b-[#f3f5f6]" border-b-1 fixed top-0 w-full bg-white z-36 :class="{ ' shadow-lg': y > 0 }">
     <Container :dynamic="true">
       <img w-40 h-full :src="Logo" alt="logo">
       <Menu :menu="menuList" />
     </Container>
   </div>
   <main pt-14 flex-1 overflow-y-auto>
-    <Container>
-      <Carousel :items="items" />
-    </Container>
-    <Container>
-      <RouterView />
-    </Container>
+    <RouterView />
     <DefaultFooter />
   </main>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,44 @@
 <script setup lang="ts">
-import type { CardItemType } from '@/components/types'
+import type { CardItemType, SwiperItemType } from '@/components/types'
+
+const items: SwiperItemType[] = [
+  {
+    image: getImagePath('banner_0.jpg'),
+    title: 'title',
+    subTitle: 'subTitle',
+    desc: 'desc',
+  },
+  {
+    image: getImagePath('banner_1.jpg'),
+    title: 'title',
+    subTitle: 'subTitle',
+    desc: 'desc',
+  },
+  {
+    image: getImagePath('banner_2.jpg'),
+    title: 'title',
+    subTitle: 'subTitle',
+    desc: 'desc',
+  },
+  {
+    image: getImagePath('banner_3.jpg'),
+    title: 'title',
+    subTitle: 'subTitle',
+    desc: 'desc',
+  },
+  {
+    image: getImagePath('banner_4.jpg'),
+    title: 'title',
+    subTitle: 'subTitle',
+    desc: 'desc',
+  },
+  {
+    image: getImagePath('banner_5.jpg'),
+    title: 'title',
+    subTitle: 'subTitle',
+    desc: 'desc',
+  },
+]
 
 const cardList: Ref<CardItemType[]> = ref([
   {
@@ -64,26 +103,33 @@ const cardList: Ref<CardItemType[]> = ref([
 </script>
 
 <template>
-  <div w-full flex-col py-8>
-    <card-header :type-id="2" />
-    <div class="grid-cols-[repeat(auto-fill,16.875rem)]" grid grid-flow-row gap-6 w-full>
-      <card v-for="(card, index) in cardList" :key="index" :card="card" img-height="h-38" width="w-67.5">
-        <template #default="{ card }">
-          <div class="text-[#9199a1]" w-full text-12px>
-            <div my-2>
-              {{ levelPipe(card.level) }}
-              <em> · </em>
-              {{ card.learnerCount }}人报名
-            </div>
-            <footer>
-              <span v-if="card.tag" class="b-[rgba(242,13,13,0.2)] bg-[rgba(242,13,13,0.6)]" px-1 b-1 rounded-0.5 mr-1 text-white>{{ tagLabelPipe(card.tag) }}</span>
-              <span v-if="card.price" text-red font-bold mr-1>{{ pricePipe(card.price) }}</span>
-              <span v-if="card.oPrice" class="text-[#6D7278]" line-through>{{ pricePipe(card?.oPrice) }}</span>
-            </footer>
-          </div>
-        </template>
-      </card>
+  <div w-full flex-col>
+    <div bg-white py-8>
+      <Container>
+        <Carousel :items="items" />
+      </Container>
     </div>
+    <Container flex-col py-7.5>
+      <card-header :type-id="2" />
+      <div class="grid-cols-[repeat(auto-fill,16.875rem)]" grid grid-flow-row gap-6 w-full>
+        <card v-for="(card, index) in cardList" :key="index" :card="card" img-height="h-38" width="w-67.5">
+          <template #default="{ card }">
+            <div class="text-[#9199a1]" w-full text-12px>
+              <div my-2>
+                {{ levelPipe(card.level) }}
+                <em> · </em>
+                {{ card.learnerCount }}人报名
+              </div>
+              <footer>
+                <span v-if="card.tag" class="b-[rgba(242,13,13,0.2)] bg-[rgba(242,13,13,0.6)]" px-1 b-1 rounded-0.5 mr-1 text-white>{{ tagLabelPipe(card.tag) }}</span>
+                <span v-if="card.price" text-red font-bold mr-1>{{ pricePipe(card.price) }}</span>
+                <span v-if="card.oPrice" class="text-[#6D7278]" line-through>{{ pricePipe(card?.oPrice) }}</span>
+              </footer>
+            </div>
+          </template>
+        </card>
+      </div>
+    </Container>
   </div>
 </template>
 
